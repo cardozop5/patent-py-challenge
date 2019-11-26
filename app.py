1eafde93fd6d3cae3d133a6e5e180f8c85cc622b
ricochet <eashaw8@gmail.com>
2019-11-25 21:45:26 -0600

    fix

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 # render out an index page
 
 @app.route("/")
-
 def home():
     map_html = open('templates/patentmap.html', 'r').read()
     return render_template("index1.html", map_html = Markup(map_html))
