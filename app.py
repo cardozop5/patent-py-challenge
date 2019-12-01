93e148b5637a46e6b6a68a83acf70ffd57743900
njokuann <njokuann@gmail.com>
2019-12-01 08:42:34 -0600

    push

--- app.py
+++ app.py
@@ -9,8 +9,15 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     map_html = open('templates/patentmap.html', 'r').read()
-    return render_template("index1.html", map_html = Markup(map_html))
+    return render_template("index2.html", map_html = Markup(map_html))
 
+@app.route("/homepage")
+def homepage():
+    return render_template("index1.html")
+
+@app.route("/states")
+def states():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
